74169ab1a7cd6a9b5db2f71c1b0b362fa970ba6d
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:42:17 +0530

    Add files via upload

---
+++ Emp_details JOINS 17-10-2024.sql
@@ -0,0 +1,19 @@
+Create table Emp_details(Emp_id char(8),Emp_name varchar(20),Department varchar(20),Salary int);
+insert into emp_details values
+('XLR-1001','Raj','Sales',100000),
+('XLR-1002','Ajay','Sales',90000),
+('XLR-1003','Viajy','Sales',80000),
+('XLR-1004','Rani','Sales',50000),
+('XLR-1005','Riya','Sales',40000),
+('XLR-1006','Priya','Marketing',100000),
+('XLR-1007','John','Marketing',95000),
+('XLR-1008','Ali','Marketing',80000),
+('XLR-1009','Omar','Marketing',100000),
+('XLR-1010','Kiran','HR',90000),
+('XLR-1011','Naresh','HR',40000),
+('XLR-1012','Peter','HR',90000),
+('XLR-1013','Laxmi','Finance',120000),
+('XLR-1014','Krishna','Finance',80000),
+('XLR-1015','Rao','Finance',60000),
+('XLR-1016','Prasad','Finance',90000),
+('XLR-1017','Susmitha','HR',100000);
